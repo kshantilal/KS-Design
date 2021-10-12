bcef188068d937d21db09b77c2e242fd041cd51e
Kunal <kshantilal03@gmail.com>
2021-10-12 22:32:39 +1300

    finished about section

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,16 +1,14 @@
 jQuery(function($){
 
 
-// Change text colour and logo on scroll for secondary navigation
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
+    // Change text colour and logo on scroll for secondary navigation
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
 
-    if (scroll >= 150) {
-        console.log('here');
-        $('nav').addClass('nav-background');
-    }else{
-        $('nav').removeClass('nav-background')
-    }
-});
-
+        if (scroll >= 150) {
+            $('nav').addClass('nav-background');
+        }else{
+            $('nav').removeClass('nav-background')
+        }
+    });
 });
--- front-page.php
+++ front-page.php
@@ -8,7 +8,6 @@
                         <h2 id="header-job-title">UX & UI Designer</h2>
                         <h3 id="header-job-title-2">Front-end Developer</h3>
                         <a href="https://www.linkedin.com/in/kunalshantilal/" target="_blank"><i class="fab fa-2x fa-linkedin"></i></a><a href="https://github.com/kshantilal" target="_blank"><i class="fab fa-2x fa-github-square"></i></a>
-                        
                     </div>
                 </div>
             </div>
@@ -24,14 +23,98 @@
                 </div>
                 <div class="col-sm-12 col-md-8">
                     <div id="profile-content">
-                        <h1 class="">about.</h1>
+                        <h1>about.</h1>
                         <p>I am a passionate Digital Designer with over 4 years of experience in a multitude of areas including front-end web development, digital design, content creation, and POS. Advanced HTML and CSS, JavaScript and jQuery, PHP and WordPress CMS development as well as Adobe Creative Suite, are some of the key areas I have technical design expertise.</p>
                         <div class="text-center text-md-left">
                             <a class="btn" href="">Download CV</a>
                         </div>
                     </div>
                 </div>
-                    
+            </div>
+        </div>
+        <div class="container">
+            <div class="row row-cols-auto skills-container">
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/photoshop.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/illustrator.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/xd.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/indesign.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/aftereffects.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/premiere_pro.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/figma.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/html.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/css.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/js.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/php.svg' ?>" alt="">
+                    </div>
+                </div>
+                <div class="col-4 col-sm-2 col-md-1">
+                    <div class="skills">
+                        <img src="<?php echo get_template_directory_uri() . '/public/img/wordpress.svg' ?>" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div id="other">
+                        <h2>other digs</h2>
+                        <p>jQuery | Node.js | Sass | Bootstrap | Jira | SEO | Google Analytics</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div id="my-work">
+                        <h1>my work.</h1>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
