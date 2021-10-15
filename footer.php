c3e9f9409f5a1d0c7df45cbd7e7d94f7873eb6bf
Kunal <kshantilal03@gmail.com>
2021-10-15 21:51:21 +1300

    styled footer

--- footer.php
+++ footer.php
@@ -1,20 +1,16 @@
 
         </main>
-        <footer role="contentinfo">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <footer id="footer-container" role="contentinfo">
+            <div class="container-fluid d-flex h-100 text-center">
+                <div class="row align-items-center flex-wrap w-100">
+                    <div class="col-12 mx-auto">
+                        <div id="footer-content">
+                            <p><i class="far fa-copyright"></i> 2021 Designed & Developed by Kunal Shantilal</p>
+                            <p>Created using a custom Wordpress theme</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     </body>
     <?php wp_footer(); ?>
