e011c3a00bf28bd8fab5f0dc5d4201249470b391
Kunal <kshantilal03@gmail.com>
2021-10-22 17:07:46 +1300

    added ga and gtm codes

--- header.php
+++ header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-nz" dir='ltr'>
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M4QG39S');</script>
+        <!-- End Google Tag Manager -->
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-X0NKH72GSJ"></script>
         <script>
@@ -8,7 +15,7 @@
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-X0NKH72GSJ');
+        gtag('config', 'G-X0NKH72GSJ'); 
         </script>
         <script src="https://kit.fontawesome.com/edcfecaa0e.js" crossorigin="anonymous"></script>
         <title>KS Design</title>
@@ -79,4 +86,9 @@ if( is_front_page() ){
     <?php endif;?>
 </header>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4QG39S"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <main role="main">
+        
